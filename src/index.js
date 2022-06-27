ee8940ccf82f4d5ffc9e5c7bc23c8c7dd2a977b4
minhphung97 <nmphung97@gmail.com>
2022-06-27 20:49:58 +0700

    Keep calm and commit

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
-const express = require("express");
-const morgan = require("morgan");
-const handlebars = require("express-handlebars");
-const path = require("path");
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+const route = require('./route');
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 //Template engine
 app.engine(
-  "hbs",
-  handlebars.engine({
-    extname: "hbs",
-  })
+    'hbs',
+    handlebars.engine({
+        extname: 'hbs',
+    }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+//Route init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
---
+++ src/route/index.js
@@ -0,0 +1,15 @@
+const newsRoute = require('./news');
+
+function route(app) {
+    app.use('/news', newsRoute);
+
+    app.get('/', (req, res) => {
+        res.render('home');
+    });
+
+    app.get('/search', (req, res) => {
+        res.render('search');
+    });
+}
+
+module.exports = route;
